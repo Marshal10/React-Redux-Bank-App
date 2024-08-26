c4f314462c51dd64f85039f5838023ffc70cbaa1
Marshal10 <marshalmaleane123@gmail.com>
2024-08-26 19:47:05 +0530

    Controlled inputs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
     style: "currency",
@@ -18,17 +20,29 @@ function App() {
 }
 
 function CreateCustomer() {
+  const [fullName, setFullName] = useState("");
+  const [nationalId, setNationalId] = useState("");
   return (
     <div>
       <h2>Create new customer</h2>
       <div className="inputs">
         <div>
           <label htmlFor="full_name">Customer full name</label>
-          <input type="text" id="full_name" />
+          <input
+            type="text"
+            id="full_name"
+            value={fullName}
+            onChange={(e) => setFullName((n) => e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="national_id">National ID</label>
-          <input type="text" id="national_id" />
+          <input
+            type="text"
+            id="national_id"
+            value={nationalId}
+            onChange={(e) => setNationalId((i) => e.target.value)}
+          />
         </div>
         <button>Create new customer</button>
       </div>
@@ -41,14 +55,28 @@ function Customer() {
 }
 
 function AccountOperations() {
+  const [depositAmt, setDepositAmt] = useState("");
+  const [currency, setCurrency] = useState("USD");
+  const [withdrawAmt, setWithdrawAmt] = useState("");
+  const [loanAmt, setLoanAmt] = useState("");
+  const [loanPurpose, setLoanPurpose] = useState("");
+
   return (
     <div>
       <h2>Create new customer</h2>
       <div className="inputs">
         <div>
           <label htmlFor="deposit">Deposit</label>
-          <input type="number" id="deposit" />
-          <select>
+          <input
+            type="number"
+            id="deposit"
+            value={depositAmt}
+            onChange={(e) => setDepositAmt((da) => e.target.value)}
+          />
+          <select
+            value={currency}
+            onChange={(e) => setCurrency((c) => e.target.value)}
+          >
             <option value="USD">US Dollar</option>
             <option value="EUR">Euro</option>
             <option value="GBP">British Pound</option>
@@ -57,13 +85,29 @@ function AccountOperations() {
         </div>
         <div>
           <label htmlFor="withdraw">Withdraw</label>
-          <input type="number" id="withdraw" />
+          <input
+            type="number"
+            id="withdraw"
+            value={withdrawAmt}
+            onChange={(e) => setWithdrawAmt((wa) => e.target.value)}
+          />
           <button>Withdraw</button>
         </div>
         <div>
           <label htmlFor="loan">Request loan</label>
-          <input type="number" id="loan" placeholder="Loan amount" />
-          <input type="text" placeholder="Loan purpose" />
+          <input
+            type="number"
+            id="loan"
+            placeholder="Loan amount"
+            value={loanAmt}
+            onChange={(e) => setLoanAmt((la) => e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Loan purpose"
+            value={loanPurpose}
+            onChange={(e) => setLoanPurpose((lp) => e.target.value)}
+          />
           <button>Request Loan</button>
         </div>
       </div>
