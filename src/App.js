59610a3844bd6e198d1579f79053dd0e66bd7b9c
Marshal10 <marshalmaleane123@gmail.com>
2024-08-26 19:28:25 +0530

    Create cusomer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      <CreateCustomer />
+    </div>
+  );
+}
+
+function CreateCustomer() {
+  return (
+    <div>
+      <h2>Create new customer</h2>
+      <div className="inputs">
+        <div>
+          <label htmlFor="full_name">Customer full name</label>
+          <input type="text" id="full_name" />
+        </div>
+        <div>
+          <label htmlFor="national_id">National ID</label>
+          <input type="text" id="national_id" />
+        </div>
+        <button>Create new customer</button>
+      </div>
     </div>
   );
 }
